75c73b0e1c531a77f652ba0965c157ba4443d441
moses <mking78911@gmail.com>
2023-01-28 07:37:36 +0000

    updated picture

--- starter/assets/js/script.js
+++ starter/assets/js/script.js
@@ -3,37 +3,34 @@ const today = moment();
 
 window.setInterval(function () {
     $('#currentDay').html(moment().format('dddd Do, MMMM YYYY, hh:mm:ss'));
-    currentTime = moment().hour();
-    taskedColours();
 }, 1000);
 
+currentTime = moment().format('H');
 
 // add event listener to the save button on click
 $('.saveBtn').on('click',function(){
     var inputValue = $(this).siblings('.description').val();
     var hour = $(this).parent().attr('id');
 
-
     //save to local storage
-    localStorage.setItem('hour', inputValue);
-
-    var savedInput = localStorage.getItem('hour');
-
-    var inputValue = $(this)
-    
-    
+    localStorage.setItem(hour, inputValue);
 });
 
+// retrieve the inputValue from local storage once page is loaded
+$(document).ready(function(){
+    $('.row').each( function(){
+        var hour = $(this).attr('id');
+        var task = localStorage.getItem(hour);
+        $(this).find('.description').val(task);
+    });
+});
 
-    
 var taskedColours = function(){
-    
-    
     $('.row').each( function(){
         var time = parseInt($(this).attr('id'));
         $(this).removeClass('past present future');
-        
-        if(time < currentTime){
+
+        if (time < currentTime){
             $(this).addClass('past');
         }
 
